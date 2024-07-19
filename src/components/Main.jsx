bbe5125a0ea9bc6be07f5bd576e15545726968bb
Beccatoni <rbccmtni1@gmail.com>
2024-07-19 15:11:39 +0200

    main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,18 @@
-
+import logo2 from '../assets/logo2.png'
 
 const Main = () => {
   return (
-    <div>
-        <h1>Fun facts about React</h1>
-        <ul>
+    <main className="bg-[#282D35] text-white font-inter h-screen flex flex-col py-14 px-7  relative">
+        <img src={logo2} alt="" className='h-72 w-56 absolute right-0 top-32' />
+        <h1 className="text-4xl font-bold m-0 spacing">Fun facts about React</h1>
+        <ul className="flex flex-col text-xl list-disc mt-12 gap-4 ml-16 max-w-[30rem] marker:text-sky-500 marker:text-[1.5rem]">
             <li>Was first released in 2019</li>
-            <li>Was </li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars  on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </div>
+    </main>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import logo from '../assets/react.svg'
 const Navbar = () => {
   return (
     <div>
-        <nav className='bg-[#21222A] h-24 flex justify-between items-center px-12 py-20'>
+        <nav className='bg-[#21222A] h-24 flex justify-between items-center px-12 py-20 font-inter'>
             <div className='flex'>
             <img src={logo} alt="" className=''/>
-            <p className='text-[#00D8FF] text-4xl font-bold font-inter'>ReactFacts</p>
+            <p className='text-[#00D8FF] text-4xl font-bold '>ReactFacts</p>
             </div>
-            <p className='text-white text-2xl font-semibold'>React Course - Project 1</p>
+            <p className='text-white text-2xl font-semibold '>React Course - Project 1</p>
         </nav>
         
     </div>

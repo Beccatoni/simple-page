b09295d41a112f7a3636822bf9e95b8a6f237c45
Beccatoni <rbccmtni1@gmail.com>
2024-07-30 15:03:57 +0200

    adding dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
+import { useState } from 'react'
+
+
 function App() {
+ const [darkMode, setDarkMode] = useState(true)
  
 
   return (
     <>
-      <Navbar/>
-      <Main/>
+      <Navbar mode={darkMode} />
+      <Main mode={darkMode} />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 
 import logo from '../assets/react.svg'
+import darkk from '../assets/dark.png'
+import light from '../assets/light.png'
+const Navbar = (props) => {
 
-const Navbar = () => {
+
   return (
     <div>
         <nav className='bg-[#21222A] h-24 flex justify-between items-center px-12 py-20 font-inter'>
@@ -9,7 +12,13 @@ const Navbar = () => {
             <img src={logo} alt="" className=''/>
             <p className='text-[#00D8FF] text-4xl font-bold '>ReactFacts</p>
             </div>
-            <p className='text-white text-2xl font-semibold '>React Course - Project 1</p>
+            <div className='flex items-center gap-2'>
+            {/* <p className='text-white text-2xl font-semibold '>React Course - Project 1</p> */}
+            <p>Light</p>
+              {props.mode? <img src={darkk} alt="" /> : <img src={light} alt="" />}
+            <p>Dark</p>
+            </div>
+            
         </nav>
         
     </div>
